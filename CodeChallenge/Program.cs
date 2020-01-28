d89c521372b6137036ef69615e377a605cc74747
Sydnee Christensen <sydnec4@byu.edu>
2020-01-27 21:43:38 -0700

    Req 2

--- CodeChallenge/Program.cs
+++ CodeChallenge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,74 +8,67 @@ using System.Threading.Tasks;
 namespace CodeChallenge
 {
     //created by: Sydnee Christensen
+    //January 2020
+
     class Program
     {
         static void Main(string[] args)
         {
 
                 //Ask user for numbers
-                Console.WriteLine("What numbers would you like to add together? Please only give two numbers and seperate the numbers by a comma.");
+                Console.WriteLine("What numbers would you like to add together? Please seperate the numbers by a comma.");
                 var line = Console.ReadLine();
 
                 //split the numbers
                 var data = line.Split(',');
-                
-                // create the two variables
-                var num1 = 0;
-                var num2 = 0;
 
-            //The if statements that will change any string inputs or empty intputs into a 0
-            if (data[0]=="")
-            {
-                num1 = 0;
-                num2 = 0;
-            }
-            else if (data.Count()== 1)
-            {
-                num1 = int.Parse(data[0]);
-            }
-            else
-            {
-                if (Int32.TryParse(data[0], out _))
+            int iCount = 0;
+            //Create an empty arraylist
+            ArrayList numbers = new ArrayList();
+            //foreach loop to take out any empty inputs or replace letters with zeros
+            foreach (string item in data)
                 {
-                    num1 = int.Parse(data[0]); 
+                if (item == "")
+                {
+                    numbers.Add(0);
+                }
+
+                else if (Int32.TryParse(data[iCount], out _))
+                {
+                    numbers.Add(int.Parse(data[iCount]));
                 }
                 else
                 {
-                    num1 = 0;
-                    
+                    numbers.Add(0);
+
                 }
 
-                if (Int32.TryParse(data[1], out _))
-                {
-                    num2 = int.Parse(data[1]); 
-                }
-                else
-                {
-                    num2 = 0;
-                    
-                }
-
+                iCount++;
             }
 
-            //The if statement to stop the user if they input more than 2 inputs
-                int c = data.Count();
-                if (c > 2)
-                {
-                    Console.WriteLine("Please only enter two numbers.");
-                    Console.ReadLine();
-                }
-                else
-                {
-                   //adding the numbers together 
-                    int answer = num1 + num2;
-                    
-                    //printing the formula
-                    Console.WriteLine(num1.ToString() + " + " + num2.ToString() + " = " + answer.ToString());
-                    Console.ReadLine();
-                }
-
-
+            iCount = 0;
+            int total = 0;
+            //Combine the values for a total
+            foreach(int item in numbers)
+                    {
+                        total = item + total;
+                    }
+                    iCount = 0;
+                    int plusSigns = 1;
+                    //loop to print out the numbers and the total
+                    foreach (int item in numbers)
+                    {
+                        Console.Write(numbers[iCount].ToString());
+                        iCount++;
+                      if (plusSigns < numbers.Count)
+                         {
+                            Console.Write(" + ");
+                            plusSigns++;
+                        };
+                                 
+                    }
+            Console.Write(" = " + total);
+            Console.ReadLine();
 
         }
 
